ca1f4aef7b37b0c416c46b0b12efe5a12b7b1c22
Dauragtiwari <www.anuragtiwari23@gmail.com>
2022-10-01 13:04:21 +0530

    Added 3 files

---
+++ Basic Python Pratice Program/3_Greater among Three Numbers.py
@@ -0,0 +1,5 @@
+first_number=int(input('Enter the first number'))
+second_number=int(input('Enter the second number'))
+third_number=int(input('Enter the third number'))
+
+print(max(first_number,second_number,third_number))
